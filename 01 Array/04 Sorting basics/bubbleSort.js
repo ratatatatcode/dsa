f69ac2e862a12eb6dc08614928362c7ed4914a14
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-08-22 02:14:53 +0800

    Add GPT response

--- 01 Array/04 Sorting basics/bubbleSort.js
+++ 01 Array/04 Sorting basics/bubbleSort.js
@@ -9,3 +9,22 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 console.log(arr);
+
+/* 
+const arr = [5, 1, 7, 4, 2, 10, 9, 3, 6, 8];
+
+for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // swap
+            swapped = true;
+        }
+    }
+    if (!swapped) break; // stop if already sorted
+}
+console.log(arr); // [1,2,3,4,5,6,7,8,9,10]
+
+It will only stop when no swaps occur inside the inner loop.
+If swapped remains false, then the loop will break.
+*/
